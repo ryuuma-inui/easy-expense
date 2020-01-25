07142313dde6e0cb189da6e1730b4fb32c3e8344
ryuma.inui <09feng07@gmail.com>
2020-01-25 17:20:22 +0000

    price validate

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,7 @@ class Expense < ApplicationRecord
     belongs_to :user
     validates :memo, presence: true, length: { maximum: 255 }
     validates :date, presence: true
-    validates :price, presence: true
+    validates :price, presence: true, length: { maximum: 20 },
+                numericality: true
     
 end
