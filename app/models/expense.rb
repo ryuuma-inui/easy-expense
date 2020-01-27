f3e67752958b53e665208de4ee288d74265f0af4
ryuma.inui <09feng07@gmail.com>
2020-01-27 07:13:37 +0000

    date validate2

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,10 +1,8 @@
 class Expense < ApplicationRecord
     belongs_to :user
     validates :memo, presence: true, length: { maximum: 255 }
-    validates :date, presence: true
     validates :price, presence: true, length: { maximum: 20 },
                 numericality: true
-    
-    # 日付のバリデーション参考
-    # https://railsguides.jp/active_record_validations.html#%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89
+    validates :date, presence: true
+   
 end
